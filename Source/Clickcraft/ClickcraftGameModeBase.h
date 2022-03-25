3844514502fb50d9173105a9b9c84dac8be28b2e
Vlajd <boriskostadinov2005@gmail.com>
2022-03-25 23:07:41 +0100

    Initial Commit 	new file:   Clickcraft.uproject 	new file:   Config/DefaultEditor.ini 	new file:   Config/DefaultEngine.ini 	new file:   Config/DefaultGame.ini 	new file:   Config/DefaultInput.ini 	new file:   Content/Audio/UI/Click.uasset 	new file:   Content/Audio/UI/ClickCue.uasset 	new file:   Content/Blueprints/BP_Block.uasset 	new file:   Content/Blueprints/BP_GameMode.uasset 	new file:   Content/Blueprints/BP_Player.uasset 	new file:   Content/Blueprints/BP_PlayerController.uasset 	new file:   Content/Blueprints/BP_UI.uasset 	new file:   Content/Blueprints/Blocks/BP_Bedrock.uasset 	new file:   Content/Blueprints/Blocks/BP_Dirt.uasset 	new file:   Content/Blueprints/Blocks/BP_Glass.uasset 	new file:   Content/Blueprints/Blocks/BP_Grass.uasset 	new file:   Content/Blueprints/Blocks/BP_Log.uasset 	new file:   Content/Blueprints/Blocks/BP_Stone.uasset 	new file:   Content/Blueprints/Blocks/BP_Wood.uasset 	new file:   Content/Levels/Main.umap 	new file:   Content/Materials/Default/M_Default.uasset 	new file:   Content/Materials/Default/M_Glass.uasset 	new file:   Content/Materials/MI_Bedrock.uasset 	new file:   Content/Materials/MI_Dirt.uasset 	new file:   Content/Materials/MI_Glass.uasset 	new file:   Content/Materials/MI_Grass.uasset 	new file:   Content/Materials/MI_Log.uasset 	new file:   Content/Materials/MI_Stone.uasset 	new file:   Content/Materials/MI_Wood.uasset 	new file:   Content/Models/Cube.uasset 	new file:   Content/Textures/Models/bedrock.uasset 	new file:   Content/Textures/Models/dirt.uasset 	new file:   Content/Textures/Models/glass.uasset 	new file:   Content/Textures/Models/grass_block_side.uasset 	new file:   Content/Textures/Models/oak_log.uasset 	new file:   Content/Textures/Models/oak_planks.uasset 	new file:   Content/Textures/Models/stone.uasset 	new file:   Content/Textures/UI/Items/Barrier.uasset 	new file:   Content/Textures/UI/Items/Bedrock.uasset 	new file:   Content/Textures/UI/Items/Dirt.uasset 	new file:   Content/Textures/UI/Items/Glass.uasset 	new file:   Content/Textures/UI/Items/Grass.uasset 	new file:   Content/Textures/UI/Items/Log.uasset 	new file:   Content/Textures/UI/Items/Stone.uasset 	new file:   Content/Textures/UI/Items/Wood.uasset 	new file:   Content/Textures/UI/WidgetsNone.uasset 	new file:   Content/Textures/UI/WidgetsSelected.uasset 	new file:   Source/Clickcraft.Target.cs 	new file:   Source/Clickcraft/Clickcraft.Build.cs 	new file:   Source/Clickcraft/Clickcraft.cpp 	new file:   Source/Clickcraft/Clickcraft.h 	new file:   Source/Clickcraft/ClickcraftGameModeBase.cpp 	new file:   Source/Clickcraft/ClickcraftGameModeBase.h 	new file:   Source/ClickcraftEditor.Target.cs 	modified:   .gitignore

---
+++ Source/Clickcraft/ClickcraftGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ClickcraftGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLICKCRAFT_API AClickcraftGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
